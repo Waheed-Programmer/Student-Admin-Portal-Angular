10468c42774e9685d00fcaf161368931ac78697a
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-06-27 09:46:19 +0500

    Complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { EmployeComponent } from './employe/employe.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component:HomeComponent,path:""},
+  {component:AboutComponent,path:"About"},
+  {component:ContactComponent,path:"Contact"},
+  {component:EmployeComponent,path:"Employee"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
