60226aad89ccf64dfd45eeb54f4923e52258d81e
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-06-27 09:32:25 +0500

    Add some new component for implementions of routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeComponent } from './employe/employe.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeComponent
+    EmployeComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
