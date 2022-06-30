d0b6d521f1df3b138daf09fe61837f9fed66dea0
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-06-30 10:56:29 +0500

    Clean Project for Student Portal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeComponent } from './employe/employe.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { EmployeAboutComponent } from './employe-about/employe-about.component';
-import { AdminModule } from './admin/admin.module';
-import { UserModule } from './user/user.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PopupComponent } from './popup/popup.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+
 
 
 
@@ -25,25 +13,14 @@ import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent,
-    EmployeAboutComponent,
-    PopupComponent,
+    
    
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AdminModule,
-    UserModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatDividerModule,
-    MatFormFieldModule,
-    MatInputModule
+    
     
   ],
   providers: [],
